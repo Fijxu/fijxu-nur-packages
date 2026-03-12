e1deec4b2a6c25c877766ea5d4195ba1fd15754f
Fijxu <fijxu@nadeko.net>
2026-03-12 16:24:28 -0300

    update packages

--- pkgs/haproxy-ech/default.nix
+++ pkgs/haproxy-ech/default.nix
@@ -1,21 +1,19 @@
 {
 
   haproxy,
-  fetchgit,
+  fetchFromGitHub,
   pkgs,
-  lib,
   ...
 }:
 
 let
   openssl-ech = pkgs.callPackage ../openssl-ech { };
-  rev = "73732abfb246d2a32c33cab45ddea2642dc31587";
-  shortRev = lib.sources.shortRev rev;
-  src = fetchgit {
-    # https://github.com/haproxy/haproxy/commit/73732abfb246d2a32c33cab45ddea2642dc31587
-    url = "https://github.com/haproxy/haproxy.git";
-    inherit rev;
-    hash = "sha256-1LH2ZaWYU7cC77pn3mGYnB076gy/FcYqHvei/JbD3BY=";
+  tag = "v3.4-dev6";
+  src = fetchFromGitHub {
+    owner = "haproxy";
+    repo = "haproxy";
+    tag = tag;
+    hash = "sha256-IJk8+6DcjkintWMA03IRDDGI3pOloPGoqeLiX5KD0sQ=";
   };
 in
 
@@ -23,7 +21,7 @@ in
   openssl = openssl-ech;
 }).overrideAttrs
   (old: {
-    version = "git-${shortRev}";
+    version = tag;
     inherit src;
     buildFlags = old.buildFlags ++ [ "USE_ECH=1" ];
     env.NIX_CFLAGS_COMPILE = "-march=x86-64-v3 -flto=auto";
--- pkgs/openssl-ech/default.nix
+++ pkgs/openssl-ech/default.nix
@@ -14,13 +14,13 @@ let
     repo = "openssl";
     # https://github.com/openssl/openssl/commit/d01cd520e52ab8bcdad27496209a27022679d970
     # `feature/ech` branch
-    inherit rev;
+    rev = rev;
     hash = "sha256-CoHRiHD8gEHtoTHRVvI0vceGJ3WblKQAExTKx9smyGc=";
   };
 in
 
 (openssl.overrideAttrs {
-  version = "feature-ech-${shortRev}";
+  version = "4.0.0-feature-ech-${shortRev}";
   inherit src;
   # openssl nixpgks package has some patches that are not compatible with
   # the `feature/ech` branch
