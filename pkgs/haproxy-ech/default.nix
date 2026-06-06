f3956f103cbb765ae9b45517c7ab1ce91afd8249
Fijxu <fijxu@nadeko.net>
2026-06-06 16:33:44 -0400

    update haproxy-ech to 3.4.0

--- pkgs/haproxy-ech/default.nix
+++ pkgs/haproxy-ech/default.nix
@@ -1,18 +1,17 @@
 {
   haproxy,
-  fetchFromGitHub,
-  pkgs,
-  openssl-ech ? pkgs.callPackage ../openssl-ech { },
+  fetchurl,
+  lib,
+  openssl_4_0,
+  openssl-ech ? openssl_4_0,
   ...
 }:
 
 let
-  tag = "v3.4-dev6";
-  src = fetchFromGitHub {
-    owner = "haproxy";
-    repo = "haproxy";
-    tag = tag;
-    hash = "sha256-IJk8+6DcjkintWMA03IRDDGI3pOloPGoqeLiX5KD0sQ=";
+  version = "3.4.0";
+  src = fetchurl {
+    url = "https://www.haproxy.org/download/${lib.versions.majorMinor version}/src/haproxy-${version}.tar.gz";
+    hash = "sha256-kpj1ZcKpuopOf4nFS+LF0/2WC1swTrVRXhXSnSwV1Pc=";
   };
 in
 
@@ -20,8 +19,7 @@ in
   openssl = openssl-ech;
 }).overrideAttrs
   (old: {
-    # version = tag;
-    # inherit src;
+    inherit version;
+    inherit src;
     buildFlags = old.buildFlags ++ [ "USE_ECH=1" ];
-    env.NIX_CFLAGS_COMPILE = "-march=x86-64-v3 -flto=auto";
   })
